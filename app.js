383babacf2f5b0fa69a76cf2e13d19e28d57f482
Richard T <74796181+rtillies@users.noreply.github.com>
2024-02-15 13:47:19 -0500

    Move dummy output to separate file

--- app.js
+++ app.js
@@ -76,28 +76,6 @@ const LearnerSubmissions = [
   }
 ];
 
-/* 
-function getLearnerData(course, ag, submissions) {
-  // here, we would process this data to achieve the desired result.
-  const result = [
-    {
-      id: 125,
-      avg: 0.985, // (47 + 150) / (50 + 150)
-      1: 0.94, // 47 / 50
-      2: 1.0 // 150 / 150
-    },
-    {
-      id: 132,
-      avg: 0.82, // (39 + 125) / (50 + 150)
-      1: 0.78, // 39 / 50
-      2: 0.833 // late: (140 - 15) / 150
-    }
-  ];
-
-  return result;
-}
- */
-
 function getLearnerData(course, ag, submissions) {
   let allAssignments = createAssignmentList(course, ag)
   let allLearners = createLearnerList(submissions)
